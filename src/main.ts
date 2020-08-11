3c4933b26ca855fdbfdcc7f9f1701f7ff5ffde79
Microwavekonijn <sjorsw@protonmail.ch>
2020-08-11 15:27:50 +0200

    Added ws, improvements

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import fastifyHelmet from 'fastify-helmet/index';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
-  const app = await NestFactory.create(
+  const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  app.useWebSocketAdapter(new WsAdapter(app));
+
+  app.register(fastifyHelmet);
+  app.enableCors();
+
   await app.listen(3000);
 }
 
