4a95646f1a36416ba99fbaa711a7340cbeeb68cf
Microwavekonijn <sjorsw@protonmail.ch>
2020-08-19 17:01:29 +0200

    Small improvements

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,5 @@
 export const config = () => ({
-  http: {
-    port: parseInt(process.env.PORT, 10) || 3000,
-  },
-
-  rabbitmq: {
-    url: ['amqp://localhost:5672'],
-    queue: process.env.QUEUE || 'websocket',
-  },
+  cors: {},
 
   database: {
     mongo: {
@@ -15,6 +8,17 @@ export const config = () => ({
       port: process.env.MONGO_PORT || 27017,
       database: process.env.MONGO_DATABASE || 'ps2alerts-api',
       logging: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
     },
+  },
+
+  http: {
+    port: parseInt(process.env.PORT, 10) || 3000,
+  },
+
+  rabbitmq: {
+    url: ['amqp://localhost:5672'],
+    queue: process.env.QUEUE || 'websocket',
   },
 });
