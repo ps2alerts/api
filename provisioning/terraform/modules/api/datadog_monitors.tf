2a10293a484dfadc06da6887d633af2d1f0aed06
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-05 00:03:44 +0100

    Attempt #1 to deploy API to staging

---
+++ provisioning/terraform/modules/api/datadog_monitors.tf
@@ -0,0 +1,104 @@
+resource datadog_monitor "api_not_running" {
+  name = "PS2Alerts API not running [${var.environment}]"
+  type = "metric alert"
+  query = "sum(last_1m):avg:kubernetes.pods.running{kube_deployment:ps2alerts-api-${var.environment}} <= 0"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "API", description: "not running"})
+
+  thresholds = {
+    critical = 0
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "api"}))
+}
+
+resource datadog_monitor "api_high_mem" {
+  name = "PS2Alerts API high memory [${var.environment}]"
+  type = "metric alert"
+  query = "avg(last_5m):avg:kubernetes.memory.rss{kube_container_name:ps2alerts-api-${var.environment}} > 262144000"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "API", description: "high memory"})
+
+  thresholds = {
+    critical = 262144000
+    warning = 209715000
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "api"}))
+}
+
+resource datadog_monitor "api_high_cpu" {
+  name = "PS2Alerts API high CPU [${var.environment}]"
+  type = "metric alert"
+  query = "avg(last_10m):avg:kubernetes.cpu.usage.total{kube_container_name:ps2alerts-api-${var.environment}} > 450000000"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "API", description: "high CPU"})
+
+  thresholds = {
+    critical = 471859000 #450MB
+    warning = 419430000 #400MB
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "api"}))
+}
+
+resource datadog_monitor "api_high_errors" {
+  name = "PS2Alerts API high errors [${var.environment}]"
+  type = "log alert"
+  query = "logs(\"container_name:*api\\-${var.environment}* status:error\").index(\"*\").rollup(\"count\").last(\"10m\") > 200"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "API", description: "high errors"})
+
+  thresholds = {
+    critical = 200
+    warning = 150
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "api"}))
+}
+
+resource datadog_monitor "api_high_restarts_long" {
+  name = "PS2Alerts API high restarts (long) [${var.environment}]"
+  type = "query alert"
+  query = "avg(last_1d):anomalies(avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-api-${var.environment}}, 'agile', 2, direction='above', alert_window='last_1h', interval=300, count_default_zero='true', seasonality='hourly') >= 1"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "API", description: "high restarts (long)"})
+
+  thresholds = {
+    critical = 1
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "api"}))
+}
+
+resource datadog_monitor "api_high_restarts_short" {
+  name = "PS2Alerts API high restarts (short) [${var.environment}]"
+  type = "query alert"
+  query = "avg(last_1h):anomalies(avg:kubernetes.containers.restarts{kube_deployment:ps2alerts-api-${var.environment}}, 'agile', 2, direction='above', alert_window='last_5m', interval=20, count_default_zero='true', seasonality='hourly') >= 1"
+  message = templatefile("${path.module}/../../dd-monitor-message.tmpl", {environment: var.environment, application: "API", description: "high restarts (long)"})
+
+  thresholds = {
+    critical = 1
+  }
+
+  notify_no_data = true
+  require_full_window = false
+  no_data_timeframe = 10
+
+  tags = jsondecode(templatefile("${path.module}/../../dd-tags.tmpl", {environment: var.environment, application: "api"}))
+}
---
+++ provisioning/terraform/staging.tf
@@ -0,0 +1,24 @@
+module "api_staging" {
+  source             = "modules/api"
+  namespace          = "ps2alerts"
+  environment        = "staging"
+  identifier         = "ps2alerts-api-staging"
+  checksum_version   = var.checksum_version
+  database_user      = var.db_user
+  database_pass      = var.db_pass
+  database_host      = "ps2alerts-db"
+  database_port      = 27017
+  database_name      = "ps2alerts-staging"
+  database_pool_size = 20
+  database_debug     = false
+  rabbitmq_host      = "ps2alerts-rabbitmq"
+  rabbitmq_user      = "ps2alerts"
+  rabbitmq_pass      = var.rabbitmq_pass
+  cpu_limit          = "500m"
+  mem_limit          = "256Mi"
+  cpu_request        = "500m"
+  mem_request        = "256Mi"
+  logger_transports  = "console"
+  dd_api_key         = var.dd_api_key
+  dd_app_key         = var.dd_app_key
+}
