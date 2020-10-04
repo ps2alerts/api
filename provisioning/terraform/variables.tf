f8a92ca012881f3429216176a1e936e0e6e69758
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-05 00:34:12 +0100

    Removed unused variable

--- provisioning/terraform/variables.tf
+++ provisioning/terraform/variables.tf
@@ -2,6 +2,5 @@ variable "checksum_version" {}
 variable "db_user" {}
 variable "db_pass" {}
 variable "rabbitmq_pass" {}
-variable "redis_pass" {}
 variable "dd_api_key" {}
 variable "dd_app_key" {}
