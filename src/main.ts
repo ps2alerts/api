fe1d6f9f135f74a328686e99930c9417471cb19a
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-07 12:24:47 +0100

    Open the taps a bit more

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap(): Promise<void> {
                 durable: true,
             },
             noAck: false,
-            prefetchCount: 200,
+            prefetchCount: 250,
         },
     });
 
