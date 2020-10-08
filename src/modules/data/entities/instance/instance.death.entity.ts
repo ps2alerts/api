b68c3e1081db4b865d60c41b9bf43c12f0cecc94
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-08 22:01:52 +0100

    Fixed incorrect index

--- src/modules/data/entities/instance/instance.death.entity.ts
+++ src/modules/data/entities/instance/instance.death.entity.ts
@@ -5,7 +5,7 @@ import {Loadout, loadoutArray} from '../../constants/loadout.consts';
 @Entity({
     name: 'instance_deaths',
 })
-@Index(['instance', 'attacker', 'character'], {unique: true})
+@Index(['instance', 'attacker', 'character', 'timestamp'], {unique: true})
 
 export default class InstanceDeathEntity {
     @ObjectIdColumn()
