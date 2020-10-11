968e3b01e52edc3f315dade9dbe906911fe091fa
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-11 23:53:13 +0100

    Deleted dead file

--- src/modules/data/entities/aggregate/common/vehicle.matrix.embed.ts
+++
@@ -1,7 +0,0 @@
-// /* eslint-disable @typescript-eslint/explicit-member-accessibility,@typescript-eslint/naming-convention */
-// import {Column} from 'typeorm';
-// import {Vehicle} from '../../../constants/vehicle.consts';
-//
-// export default class VehicleMatrixEmbed {
-//     [prop: Vehicle]: number;
-// }
