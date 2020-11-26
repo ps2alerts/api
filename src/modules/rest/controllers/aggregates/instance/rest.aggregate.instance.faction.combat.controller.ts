5f973dda0c141432e91dfedf59b44ae5e2d0003b
Matt Cavanagh <maelstrome26@gmail.com>
2020-11-26 01:11:57 +0000

    Actually correctly fixed instance faction aggregator endpoint

--- src/modules/rest/controllers/aggregates/instance/rest.aggregate.instance.faction.combat.controller.ts
+++ src/modules/rest/controllers/aggregates/instance/rest.aggregate.instance.faction.combat.controller.ts
@@ -1,11 +1,7 @@
-import {Controller, Get, Inject, Param, Query} from '@nestjs/common';
+import {Controller, Get, Inject, Param} from '@nestjs/common';
 import {ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger';
 import InstanceFactionCombatAggregateEntity from '../../../../data/entities/aggregate/instance/instance.faction.combat.aggregate.entity';
 import MongoOperationsService from '../../../../../services/mongo/mongo.operations.service';
-import {OptionalIntPipe} from '../../../pipes/OptionalIntPipe';
-import {ApiImplicitQueries} from 'nestjs-swagger-api-implicit-queries-decorator';
-import {PAGINATION_IMPLICIT_QUERIES} from '../../common/rest.pagination.queries';
-import Pagination from '../../../../../services/mongo/pagination';
 
 @ApiTags('Instance Faction Combat Aggregates')
 @Controller('aggregates')
@@ -15,21 +11,15 @@ export default class RestInstanceFactionCombatAggregateController {
     ) {}
 
     @Get('instance/:instance/faction')
-    @ApiOperation({summary: 'Returns a list of InstanceFactionCombatAggregateEntity for an instance'})
-    @ApiImplicitQueries(PAGINATION_IMPLICIT_QUERIES)
+    @ApiOperation({summary: 'Returns the InstanceFactionCombatAggregateEntity for an instance'})
     @ApiResponse({
         status: 200,
-        description: 'The list of InstanceFactionCombatAggregateEntity aggregates',
+        description: 'The InstanceFactionCombatAggregateEntity aggregate',
         type: InstanceFactionCombatAggregateEntity,
-        isArray: false,
     })
-    async findAll(
+    async findOne(
         @Param('instance') instance: string,
-            @Query('sortBy') sortBy?: string,
-            @Query('order') order?: string,
-            @Query('page', OptionalIntPipe) page?: number,
-            @Query('pageSize', OptionalIntPipe) pageSize?: number,
-    ): Promise<InstanceFactionCombatAggregateEntity[]> {
-        return this.mongoOperationsService.findMany(InstanceFactionCombatAggregateEntity, {instance}, new Pagination({sortBy, order, page, pageSize}));
+    ): Promise<InstanceFactionCombatAggregateEntity> {
+        return this.mongoOperationsService.findOne(InstanceFactionCombatAggregateEntity, {instance});
     }
 }
