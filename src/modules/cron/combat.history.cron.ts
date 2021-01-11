2f7d2f65ba9f235dee087d7b6953b64956e2eaf0
Matt Cavanagh <maelstrome26@gmail.com>
2021-01-11 22:17:29 +0000

    Added check for combat history cronjob for overdue alerts

--- src/modules/cron/combat.history.cron.ts
+++ src/modules/cron/combat.history.cron.ts
@@ -22,9 +22,16 @@ export class CombatHistoryCron {
         const documents = [];
 
         // Grab the current actives
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const actives: InstanceMetagameTerritoryEntity[] = await this.mongoOperationsService.findMany(InstanceMetagameTerritoryEntity, {state: Ps2alertsEventState.STARTED});
 
         for await (const row of actives) {
+            // If instance is overdue, don't process
+            if (Date.now() > (row.timeStarted.getTime() + row.duration)) {
+                this.logger.error(`Instance [${row.instanceId}] is overdue, skipping combat history job`);
+                continue;
+            }
+
             // Pull latest faction combat entity
             try {
                 const factionCombat: InstanceFactionCombatAggregateEntity = await this.mongoOperationsService.findOne(
