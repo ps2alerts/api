f2f624fe65df08d93f082bdd92440877eab7cf07
Ryan <ryanjsims@email.arizona.edu>
2022-04-04 19:07:38 -0700

    Made requested changes to OptionalBoolPipe

--- src/modules/rest/pipes/OptionalBoolPipe.ts
+++ src/modules/rest/pipes/OptionalBoolPipe.ts
@@ -3,6 +3,6 @@ import {PipeTransform, Injectable} from '@nestjs/common';
 @Injectable()
 export class OptionalBoolPipe implements PipeTransform {
     transform(value: string | undefined): boolean|undefined {
-        return value ? value === 'true' : undefined;
+        return value === 'true' ?? undefined;
     }
 }
