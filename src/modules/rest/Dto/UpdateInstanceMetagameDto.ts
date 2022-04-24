b80758f5a745212035b262e69edb5eef1a89c5bb
Matt Cavanagh <maelstrome26@gmail.com>
2022-04-24 14:49:47 +0100

    Added missing properties from update instance dto after completing on the aggregator side

--- src/modules/rest/Dto/UpdateInstanceMetagameDto.ts
+++ src/modules/rest/Dto/UpdateInstanceMetagameDto.ts
@@ -1,6 +1,9 @@
-import {IsDateString, IsNotEmpty, IsNumber, IsString} from 'class-validator';
+import {IsDateString, IsNotEmpty, IsNumber, IsObject, IsString} from 'class-validator';
 import {ApiModelProperty} from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import {Ps2alertsEventState} from '../../data/constants/eventstate.consts';
+import Ps2AlertsInstanceResultInterface from '../../../interfaces/Ps2AlertsInstanceResultInterface';
+import {Faction} from '../../data/constants/faction.consts';
+import {Bracket} from '../../data/constants/bracket.consts';
 
 export class UpdateInstanceMetagameDto {
     @IsString()
@@ -13,8 +16,28 @@ export class UpdateInstanceMetagameDto {
     @ApiModelProperty({example: Ps2alertsEventState.ENDED})
     state: Ps2alertsEventState;
 
+    @IsNumber()
+    @IsNotEmpty()
+    @ApiModelProperty({example: Bracket.PRIME})
+    bracket: Bracket;
+
     @IsDateString()
     @IsNotEmpty()
     @ApiModelProperty({example: '2022-03-27T01:00:10.463Z'})
     timeEnded: Date;
+
+    @IsObject()
+    @IsNotEmpty()
+    @ApiModelProperty({
+        example: {
+            vs: 30,
+            nc: 27,
+            tr: 40,
+            cutoff: 1,
+            outOfPlay: 0,
+            victor: Faction.TERRAN_REPUBLIC,
+            draw: true,
+        },
+    })
+    result: Ps2AlertsInstanceResultInterface;
 }
