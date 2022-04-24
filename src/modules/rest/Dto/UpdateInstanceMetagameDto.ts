59ce27735291037e16fae895236a83f40abaf650
Matt Cavanagh <maelstrome26@gmail.com>
2022-04-24 19:05:52 +0100

    Made supplying all data in UpdateInstanceMetagameDto optional so we can do partial updates

--- src/modules/rest/Dto/UpdateInstanceMetagameDto.ts
+++ src/modules/rest/Dto/UpdateInstanceMetagameDto.ts
@@ -1,4 +1,4 @@
-import {IsDateString, IsNotEmpty, IsNumber, IsObject, IsString} from 'class-validator';
+import {IsDateString, IsNotEmpty, IsNumber, IsObject, IsOptional, IsString} from 'class-validator';
 import {ApiModelProperty} from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import {Ps2alertsEventState} from '../../data/constants/eventstate.consts';
 import Ps2AlertsInstanceResultInterface from '../../../interfaces/Ps2AlertsInstanceResultInterface';
@@ -8,26 +8,31 @@ import {Bracket} from '../../data/constants/bracket.consts';
 export class UpdateInstanceMetagameDto {
     @IsString()
     @IsNotEmpty()
+    @IsOptional()
     @ApiModelProperty({example: '10-12345'})
     instanceId: string;
 
     @IsNumber()
     @IsNotEmpty()
+    @IsOptional()
     @ApiModelProperty({example: Ps2alertsEventState.ENDED})
     state: Ps2alertsEventState;
 
     @IsNumber()
     @IsNotEmpty()
+    @IsOptional()
     @ApiModelProperty({example: Bracket.PRIME})
     bracket: Bracket;
 
     @IsDateString()
     @IsNotEmpty()
+    @IsOptional()
     @ApiModelProperty({example: '2022-03-27T01:00:10.463Z'})
     timeEnded: Date;
 
     @IsObject()
     @IsNotEmpty()
+    @IsOptional()
     @ApiModelProperty({
         example: {
             vs: 30,
