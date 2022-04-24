0b6eb6bdd25ebeb7cd7e71b0764746e8d402d6aa
Matt Cavanagh <maelstrome26@gmail.com>
2022-04-24 15:03:57 +0100

    Change staging DB to use prod db

--- provisioning/terraform/staging.tf
+++ provisioning/terraform/staging.tf
@@ -8,7 +8,7 @@ module "api_staging" {
   database_pass      = var.db_pass
   database_host      = "ps2alerts-db"
   database_port      = 27017
-  database_name      = "ps2alerts-staging"
+  database_name      = "ps2alerts"
   database_pool_size = 100
   database_debug     = false
   rabbitmq_host      = "ps2alerts-rabbitmq"
