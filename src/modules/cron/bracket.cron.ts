8f7361017f7b3642cdfe6e511781ab293bbf28d7
Matt Cavanagh <maelstrome26@gmail.com>
2022-09-01 23:38:10 +0100

    Handle scenario when aggregators get rebooted and pop keys are flushed

--- src/modules/cron/bracket.cron.ts
+++ src/modules/cron/bracket.cron.ts
@@ -64,6 +64,10 @@ export class BracketCron {
                         $match: {
                             instance: instance.instanceId,
                             timestamp: {$gt: startDate},
+                            vs: {$gt: 0}, // To not count instances when aggregator has reset it's pops due to restarts
+                            nc: {$gt: 0},
+                            tr: {$gt: 0},
+                            nso: {$gt: 0},
                         },
                     }, {
                         $group: {
