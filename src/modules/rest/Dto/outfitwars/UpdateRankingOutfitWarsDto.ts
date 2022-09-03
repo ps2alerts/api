3fb3023457ffd0a8264a0d85cfdd953de13c37ef
Ryan Sims <ryanjsims@email.arizona.edu>
2022-09-02 17:18:39 -0700

    Add some more fudging enablers to the API
    cron will now insert the first malformed entry we see, then no longer update it so it can be fudged

--- src/modules/rest/Dto/outfitwars/UpdateRankingOutfitWarsDto.ts
+++ src/modules/rest/Dto/outfitwars/UpdateRankingOutfitWarsDto.ts
@@ -1,8 +1,32 @@
 import {ApiModelProperty} from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
-import {IsNotEmpty} from 'class-validator';
+import {IsOptional} from 'class-validator';
 
 export class UpdateRankingOutfitWarsDto {
-    @IsNotEmpty()
-    @ApiModelProperty({example: 'outfitwars-1-10-25939'})
-    instanceId: string;
+    @IsOptional()
+    @ApiModelProperty({example: 'outfitwars-1-10-25939', required: false})
+    instanceId?: string | undefined;
+
+    @IsOptional()
+    @ApiModelProperty({
+        example: 1, 
+        description: 'If fudging is needed due to bugs with data retrieval (Cobalt), updates the wins of a ranking for an outfit. If legitimate data exists in Falcon\'s API, it will overwrite this fudged data.',
+        required: false
+    })
+    wins?: number | undefined;
+
+    @IsOptional()
+    @ApiModelProperty({
+        example: 0,
+        description: 'Updates the losses of a ranking for an outfit.',
+        required: false
+    })
+    losses?: number | undefined;
+
+    @IsOptional()
+    @ApiModelProperty({
+        example: 420,
+        description: 'Updates the tiebreaker points of a ranking for an outfit.',
+        required: false
+    })
+    tiebreakerPoints?: number | undefined;
 }
